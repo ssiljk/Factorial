1f4e7d614b8c043fbfb281b02d1b4f22f0dead01
Jesus Leon <jesusleon@outlook.com>
2021-04-11 11:27:59 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int fac = Factorial(n);
+            Console.WriteLine("Factorial de: {0} es: {1}", n, fac );
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return (n * Factorial(n - 1));
+            
+        }
+    }
+}
